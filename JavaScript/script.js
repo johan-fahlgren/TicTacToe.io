d3571ceffc23f9a6d7f41b615677dc155afe7e68
Johan Fahlgren <johan.fahlgren@utb.ecutbildning.se>
2022-01-28 18:19:46 +0100

    Minor updates in style, UI-functions and comments

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,13 +1,24 @@
+// TODO - ADD BOT FUNCTION
+
+//Variables
 const playerOne = "x";
 const playerTwo = "circle";
+let scoreOne = 0;
+let scoreTwo = 0;
 let playerTurn;
 
+//Button Elements
+const newGame_btn = document.getElementById("newGame_btn");
 const restart_btn = document.getElementById("restart_btn");
 
+//Elements
 const cellElements = document.querySelectorAll("[data-index]");
 const board = document.getElementById("board");
 const dataWinningMessage = document.querySelector("[data-winning-message]");
 const winningMessageElement = document.getElementById("winning_message");
+const playerOneScore = document.querySelector(".playerOneScore");
+const playerTwoScore = document.querySelector(".playerTwoScore");
+const playerMessage = document.querySelector(".player_Message");
 
 // Winning patterns
 const win = [
@@ -23,10 +34,16 @@ const win = [
 
 startGame();
 
-restart_btn.addEventListener("click", startGame);
+//Button event listeners
+newGame_btn.addEventListener("click", startGame);
+restart_btn.addEventListener("click", restartGame);
 
+//Initializing the game and also clears the board from previous game data.
 function startGame() {
   playerTurn = Math.random() >= 0.5;
+  playerOneScore.textContent = scoreOne;
+  playerTwoScore.textContent = scoreTwo;
+  startMessage(playerTurn);
 
   cellElements.forEach((cell) => {
     cell.classList.remove(playerOne);
@@ -38,32 +55,63 @@ function startGame() {
   winningMessageElement.classList.remove("show");
 }
 
+// Takes in click parameter from EventListener
+// Adds currentPlayer from playerTurn
+// calls on differnt functions in game logic based on player and cell data.
 function handleClick(e) {
-  //clicked cell
   const cell = e.target;
-
   const currentPlayer = playerTurn ? playerOne : playerTwo;
-  //Place marker/tick
   placeMark(cell, currentPlayer);
 
   if (checkWin(currentPlayer)) {
+    updateScore(currentPlayer);
     endGame(false);
   } else if (isDraw()) {
     endGame(true);
   } else {
     switchTurns();
     boardHoverClass();
+    updateMessage(currentPlayer);
   }
 }
 
+//TODO - BUG: Score to currentplayer even if game ends in a draw.
+// Updates game score
+function updateScore(currentPlayer) {
+  if (currentPlayer == playerOne) {
+    playerOneScore.textContent = scoreOne++;
+  } else {
+    playerTwoScore.textContent = scoreTwo++;
+  }
+}
+
+//Initializing which player got the first move/starts.
+function startMessage(playerTurn) {
+  if (playerTurn === true) {
+    playerMessage.textContent = "X Start";
+  } else {
+    playerMessage.textContent = "Circle Start";
+  }
+}
+//Uppdates 'startMessage()' to show whos turn it is
+// based on currentplayer variable.
+function updateMessage(currentPlayer) {
+  if (currentPlayer == playerTwo) {
+    playerMessage.textContent = "X's Turn";
+  } else {
+    playerMessage.textContent = "Circle's Turn";
+  }
+}
+// Adds class to cell in order to put down an X or circle.
 function placeMark(cell, currentPlayer) {
   cell.classList.add(currentPlayer);
 }
 
+// Switches between players every turn if there is no win or draw.
 function switchTurns() {
   playerTurn = !playerTurn;
 }
-
+// Adds hover effect to cell to show current mark/player.
 function boardHoverClass() {
   board.classList.remove(playerOne);
   board.classList.remove(playerTwo);
@@ -74,6 +122,7 @@ function boardHoverClass() {
   }
 }
 
+// Checks trough all the cells and sees if there is a match with 'win' array.
 function checkWin(currentPlayer) {
   return win.some((combinations) => {
     return combinations.every((index) => {
@@ -82,6 +131,7 @@ function checkWin(currentPlayer) {
   });
 }
 
+// Checks if every cell has either a playerOne och playerTwo class inside.
 function isDraw() {
   return [...cellElements].every((cell) => {
     return (
@@ -90,6 +140,7 @@ function isDraw() {
   });
 }
 
+// Outputs winning message based on a bool from check win and isdraw result.
 function endGame(draw) {
   if (draw) {
     dataWinningMessage.textContent = "Draw!";
@@ -97,4 +148,9 @@ function endGame(draw) {
     dataWinningMessage.textContent = `${playerTurn ? "X's" : "O's"} Win!`;
   }
   winningMessageElement.classList.add("show");
+}
+
+// Reloads current html document.
+function restartGame() {
+  return location.reload(true);
 }
